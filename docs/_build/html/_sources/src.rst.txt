2c533966380564cc99110fcbe7b1089e921efea7
Sofia Gerard <sgerardr@gmail.com>
2025-02-23 18:25:00 -0600

    finally sphinx working

---
+++ docs/_build/html/_sources/src.rst.txt
@@ -0,0 +1,37 @@
+src package
+===========
+
+Submodules
+----------
+
+src.data\_utils module
+----------------------
+
+.. automodule:: src.data_utils
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+src.feature\_engineering\_utils module
+--------------------------------------
+
+.. automodule:: src.feature_engineering_utils
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+src.model\_utils module
+-----------------------
+
+.. automodule:: src.model_utils
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Module contents
+---------------
+
+.. automodule:: src
+   :members:
+   :undoc-members:
+   :show-inheritance:
--- docs/conf.py
+++ docs/conf.py
@@ -1,28 +1,38 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
 
+# 📌 Asegurar que Sphinx pueda encontrar los módulos en `src/`
+sys.path.insert(0, os.path.abspath('../'))  # Sube un nivel para encontrar `src/`
+
+# -- Información del Proyecto -------------------------------------------------
 project = 'docker_housing_hw'
 copyright = '2025, Sofia Gerard'
 author = 'Sofia Gerard'
 release = '0.1'
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+# -- Configuración General ---------------------------------------------------
+extensions = [
+    "sphinx.ext.autodoc",        # 📌 Extrae documentación de docstrings automáticamente
+    "sphinx.ext.napoleon",       # 📌 Soporte para docstrings Google y NumPy
+    "sphinx.ext.viewcode",       # 📌 Agrega enlaces al código fuente
+    "sphinx.ext.autosummary",    # 📌 Genera resúmenes automáticos
+    "sphinx_autodoc_typehints",  # 📌 Muestra anotaciones de tipo en la documentación
+]
 
-extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 language = 'es'
 
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'alabaster'
+# -- Opciones de Salida en HTML -----------------------------------------------
+html_theme = "sphinx_rtd_theme"  # Usar ReadTheDocs como tema
 html_static_path = ['_static']
+
+# -- Configuración de Napoleon ------------------------------------------------
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = False  # Excluye docstrings de __init__
+napoleon_use_param = True
+napoleon_use_rtype = False
